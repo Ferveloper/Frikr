6b3a29b23cef0e1390600fad4c904c9ebb03a1e4
Ferveloper <fermervil@gmail.com>
2019-05-11 18:27:56 +0200

    Add visibility field to photos

--- photos/models.py
+++ photos/models.py
@@ -27,6 +27,7 @@ class Photo(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     modification_date = models.DateTimeField(auto_now=True)
     license = models.CharField(max_length=3, choices=LICENSES)
+    visibility = models.CharField(max_length=3, choices=VISIBILITY, default=PUBLIC)
 
     def __str__(self):
         return self.name
